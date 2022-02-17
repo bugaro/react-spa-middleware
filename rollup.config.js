e8a1cc3c5cc768f5387624c86dc1ea8973bb0156
Roman Ivanov <varejy@gmail.com>
2022-02-18 01:58:01 +0200

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import typescript from 'rollup-plugin-typescript2';
 import packageJson from './package.json';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: './src/index.ts',
@@ -13,5 +14,16 @@ export default {
       sourcemap: true,
     },
   ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: 'tsconfig.json' })],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: 'tsconfig.json' }),
+    copy({
+      targets: [
+        { src: 'package.json', dest: 'dist' },
+        { src: 'src/@types', dest: 'dist' },
+      ],
+    }),
+  ],
 };
